fa40a3f63b9a25b80da9658f2a9a47f7acacc30c
max-im <pogidaevmo@gmail.com>
2025-03-04 00:37:47 +0200

    implement pet checkout

--- actions/pets.ts
+++ actions/pets.ts
@@ -6,24 +6,47 @@ import { db } from '@/db';
 
 export const fetchPets = async (): Promise<Pet[]> => {
     return await db.pet.findMany({});
-} 
+}
 
 export const fetchPetsByName = async (q: string): Promise<Pet[]> => {
     return await db.pet.findMany({
-        where: { OR: [
-            { name: { contains: q } },
-        ]},
+        where: {
+            OR: [
+                { name: { contains: q } },
+            ]
+        },
     });
-} 
+}
 
 export const fetchPetsNum = async (): Promise<number> => {
     return await db.pet.count();
 }
 
+export interface CheckoutPetState {
+    errors: string[],
+    success?: boolean;
+}
+
 export const checkoutPet = async (petId: string) => {
-    await db.pet.delete({
-        where: { id: petId }
-    });
-    
+    try {
+        await db.pet.delete({ where: { id: petId } });
+    } catch (err) {
+        const defaultError = 'Checkout pet error';
+
+        if (err instanceof Error) {
+            return {
+                errors: [err.message || defaultError]
+            }
+        } else {
+            return {
+                errors: [defaultError]
+            }
+        }
+    }
+
     revalidatePath('/app/dashboard');
+    return {
+        success: true,
+        errors: []
+    };
 }
--- components/CheckoutPet/CheckoutPet.tsx
+++ components/CheckoutPet/CheckoutPet.tsx
@@ -1,12 +1,34 @@
+'use client';
+
+import { useActionState } from "react";
 import { checkoutPet } from "@/actions";
 import { Button } from "../ui/button";
 
 export default function CheckoutPet({ petId }: { petId: string }) {
+  const [formState, action, isPending] = useActionState(
+    checkoutPet.bind(null, petId),
+    { errors: [] }
+  );
+
+  if (isPending) {
+    return (<Button variant="secondary" disabled>
+      Checkout
+    </Button>)
+  }
+
   return (
-    <form action={checkoutPet.bind(null, petId)}>
-      <Button variant="secondary" type="submit">
-        Checkout
-      </Button>
-    </form>
+    <div className="relative">
+      <form action={action}>
+        <Button variant="secondary" type="submit">
+          Checkout
+        </Button>
+
+      </form>
+      {formState.errors.length ? (
+        <div className="absolute top-[100%] p-2 bg-red-200 border rounded border-red-400">
+          {formState.errors?.join(", ")}
+        </div>
+      ) : null}
+    </div>
   )
 }
